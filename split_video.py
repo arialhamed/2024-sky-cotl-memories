70d5a28115a3d15e93d2f4acac9e34808273ccd6
arialhamed <arifhamed.arial@outlook.com>
2024-08-04 23:37:14 +0800

    08-04-2024 23:37:14

---
+++ split_video.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# This script splits videos into a set size. Default here splits videos to 93M, configurable. 
+# Works in Linux, may work for Windows & Mac if mkvtools (mkvmerge) is in PATH
+
+import os
+# from tqdm import tqdm
+
+deans_list = [x for x in os.popen('f(){ find "$@" -type f -size +80M ; unset -f f; }; f').read().split('\n') if os.path.isfile(x)]
+for i in [x for x in deans_list if x.endswith(".mp4") or x.endswith(".mkv")]:
+	print(f"CONSOLE: PROCESSING \"{i}\"")
+	os.system(f"mkvmerge -o \"{i.rsplit('.',1)[0]} (split).{i.rsplit('.',1)[-1]}\" --split 80M \"{i}\"")
+	os.system(f"rm -rf \"{i}\"")
+
+os.system("notify-send \"File finished\" --urgency=critical")
